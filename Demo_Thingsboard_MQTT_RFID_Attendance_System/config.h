b82fcd82b4a733b39a77d2df5288f694bbe63558
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2025-07-03 10:42:17 +0530

    Add files via upload

---
+++ Demo_Thingsboard_MQTT_RFID_Attendance_System/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi Credentials
+#define WIFI_SSID     "Prateek"
+#define WIFI_PASSWORD "justdoelectronics@#12345"
+
+// ThingsBoard MQTT Server Settings
+#define THINGSBOARD_SERVER   "demo.thingsboard.io"
+#define THINGSBOARD_PORT     1883
+#define ACCESS_TOKEN         "vypuqubzxn9t2wne7y"
+
+// RFID Pins
+#define SS_PIN  2   
+#define RST_PIN 15  
+
+// Hardware Pins
+#define BUZZER_PIN      33
+#define MQTT_LED_PIN    14    
+#define STATUS_LED_PIN  12   
+
+#endif
